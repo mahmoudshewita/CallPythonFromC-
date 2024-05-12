edf78f5042384de74075b22aa52cde185dc8a12f
Mahmoud Shewita <57547096+mahmoudshewita@users.noreply.github.com>
2024-05-12 22:02:42 +0400

    changes

--- PDFReader/Program.cs
+++ PDFReader/Program.cs
@@ -5,13 +5,14 @@ Runtime.PythonDLL = @"C:\Python312\python312.dll";
 PythonEngine.Initialize();
 using (Py.GIL())
 {
-
+    // python file name without .py extenssion
     var pyScript = Py.Import("pdf_text_extractor");
-    // call pthon without parameter
+
+    // call pthon function without parameter
     var res = pyScript.InvokeMethod("hello");
     Console.WriteLine(res);
 
-    // call pthon with parameter
+    // call pthon function with parameter
     var PDFfilePath_PyFunctionParameter = new PyString("test.pdf");
     var PDFTextResult = pyScript.InvokeMethod("extractTextFromPDF", new PyObject[] { PDFfilePath_PyFunctionParameter });
     Console.Write(PDFTextResult);
